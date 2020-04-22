dcb1dd6c6d9235cbba0f56d7ddd658344111bea1
Angad Grewal <angadgrewal31@gmail.com>
2020-04-22 00:00:15 +0000

    removed a console.log that was no longer needed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
     let content = $(this).val();
     let charCount = content.length;
     let remainingChars = 140 - charCount;
-    console.log("characters in body: ", charCount, "characters remaining: ", remainingChars);
     $(".counter").empty().html(remainingChars);
     if (remainingChars >= 1) {
       $(".counter").removeClass("redtext");

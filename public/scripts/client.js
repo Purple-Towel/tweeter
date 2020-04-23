ca57422622acddf34fb9fb2854d69bcf14e6f8fe
Angad Grewal <angadgrewal31@gmail.com>
2020-04-23 02:57:54 +0000

    added Ajax tweet posting and fetching functions to client.js, edited index.html to allow for selecting the form element via id to do so

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,32 +6,6 @@
 
 $(document).ready(function() {
 
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];  
-
   const createTweetElement = function(tweetObj) {
     let { created_at, user, content } = tweetObj
     let { avatars, name, handle } = user;
@@ -72,6 +46,19 @@ $(document).ready(function() {
     }
   };
 
-  renderTweets(data);
+  const fetchTweets = function() {
+    $.ajax("/tweets/", {method: "GET"})
+    .then(function(tweets) {
+      renderTweets(tweets);
+    });
+  }
+
+  fetchTweets();
+
+  $( "#sendtweet" ).submit(function( event ) {
+    event.preventDefault();
+    let serializedData = $(this).serialize();
+    $.ajax("/tweets/", { method: "POST", data: serializedData });
+  });
 
 });

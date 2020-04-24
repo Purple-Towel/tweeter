30aefd98d32eb63188ad37fa01fe229693d8755b
Angad Grewal <angadgrewal31@gmail.com>
2020-04-24 04:41:07 +0000

    added/edited commenting on all major files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,18 +5,22 @@
  */
 
 $(document).ready(function () {
-
+  
+  // Takes in a tweet object;
   const createTweetElement = function (tweetObj) {
+    // Object notation shorthand, to prevent self repitition such as tweetObj.user.avatars and tweetObj.content.text
     let { created_at, user, content } = tweetObj
     let { avatars, name, handle } = user;
     let { text } = content;
-
+    
+    // Sanitizer function to render any potential malicious code attacks via our tweet text harmless
     const sanitzer = function(str) {
       let div = document.createElement('div');
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
     };
 
+    // Template string to construct our html to return to the DOM
     let $tweetHTML =
       `
       <article>
@@ -47,13 +51,15 @@ $(document).ready(function () {
     return $tweetHTML;
   };
 
+  // Takes in an array of tweet objects and creates the html for them, then prepends them to our html element with the tweets-container class
   const renderTweets = function (tweetsArr) {
     for (let tweet of tweetsArr) {
-      let $tweetToAppend = createTweetElement(tweet);
-      $('#tweets-container').prepend($tweetToAppend);
+      let $tweetToPrepend = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweetToPrepend);
     }
   };
 
+  // Our Ajax function for retrieving our tweets from our database; first it clears the tweets-container and then calls renderTweets on the data it fetches with our GET request
   const fetchTweets = function () {
     $('#tweets-container').empty();
     $.ajax("/tweets/", { method: "GET" })
@@ -62,8 +68,12 @@ $(document).ready(function () {
       });
   }
 
+  // We call this function once on page load and every time we hit submit
   fetchTweets();
 
+  /*
+  Listens for a submit event on our element with the sendtweet id, removes any messages inside the error element and the show-error class that our css handles rendering for; then checks the content for either of our two validation conditions and then prevents the submission from going through and passes an error message to our error element and adds the show-error class and plays an animation. If the content passes the validation, it instead does an Ajax post, then clears the text input and counter elements and refreshes the tweets on the page.
+  */
   $("#sendtweet").submit(function (event) {
     event.preventDefault();
     $(".error").empty().removeClass("show-error")
@@ -83,6 +93,7 @@ $(document).ready(function () {
     };
   });
 
+  // Toggles visibility of our compose tweet element and also focuses on the text input of said element
   $(".compose-tweet").click(function (event) {
     $(".new-tweet").slideToggle();
     $(".new-tweet").find("textarea").focus();
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  /*
+  This is a helper function that updates every time JQuery detects the user lets go of a key in our html form text input, which then checks the body length and gets the difference between it and our arbitrary character limit of 140; which it then uses to refresh our html element with the "counter" class to the current value, and finally it then checks whether or not to apply the "error" class which is defined in our .css
+  */
   $("#tweet-text").on("keyup", function() {
     let content = $(this).val();
     let charCount = content.length;
